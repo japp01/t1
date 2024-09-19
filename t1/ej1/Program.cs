4876b36491daa5de9769d04054a227b54ed7128c
japp01 <n00407782@upn.pe>
2024-09-19 08:16:45 -0500

    first problem, added process (real)

--- t1/ej1/Program.cs
+++ t1/ej1/Program.cs
@@ -1,5 +1,5 @@
 ﻿string name;
-double salarioBruto, ventas, years, totalGanado;
+double salarioBruto, ventas, years, totalGanado = 0;
 
 Console.Write("Nombre: ");
 name = Convert.ToString(Console.ReadLine());
@@ -10,4 +10,28 @@ ventas = Convert.ToDouble(Console.ReadLine());
 Console.Write("Años en empresa: ");
 years = Convert.ToDouble(Console.ReadLine());
 
+totalGanado += salarioBruto;
 
+//sumando comision
+if (ventas > 0 && ventas <=600)
+{
+    totalGanado += ventas * 0.05;
+}
+else if ( ventas > 600 && ventas <= 3000)
+{
+    totalGanado += ventas * 0.07;
+}
+else if (ventas > 3000)
+{
+    totalGanado += 300 + ventas * 0.06;
+}
+
+//descuento de impuestos
+if (years <= 3)
+{
+    totalGanado -= totalGanado * 0.065;
+}
+else if ( years >= 4)
+{
+    totalGanado -= totalGanado * 0.045;
+}
