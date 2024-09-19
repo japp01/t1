cb4e2d3eda741867dbef8bb4b1fd9412cdbb731d
japp01 <n00407782@upn.pe>
2024-09-19 08:02:11 -0500

    first problem, added inputs

--- t1/ej1/Program.cs
+++ t1/ej1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string name;
+double salarioBruto, ventas, years, totalGanado;
+
+Console.Write("Nombre: ");
+name = Convert.ToString(Console.ReadLine());
+Console.Write("Salario bruto: ");
+salarioBruto = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ventas: ");
+ventas = Convert.ToDouble(Console.ReadLine());
+Console.Write("Años en empresa: ");
+years = Convert.ToDouble(Console.ReadLine());
+
+
