d9de1bdffabf655aed4875d1497ec59fcccef529
japp01 <n00407782@upn.pe>
2024-09-19 08:39:28 -0500

    second problem, almost finished

---
+++ t1/ej2/Program.cs
@@ -0,0 +1,33 @@
+﻿char caracter;
+int verde = 0, azul = 0, rojo = 0;
+bool is_finished = false;
+do
+{
+    Console.Write("Ingrese el ultimo digito de la placa: ");
+    caracter = Convert.ToChar(Console.ReadLine());
+    switch (caracter)
+    {
+        case '1':
+        case '2':
+        case '3':
+            verde++;
+            break;
+        case '4':
+        case '5':
+        case '6':
+            azul++;
+            break;
+        case '7':
+        case '8':
+        case '9':
+            rojo++;
+            break;
+        case 'f':
+        case 'F':
+            is_finished = true;
+    }
+} while (is_finished = false);
+
+Console.WriteLine($"verde: {verde}");
+Console.WriteLine($"azul: {azul}");
+Console.WriteLine($"rojo: {rojo}");
